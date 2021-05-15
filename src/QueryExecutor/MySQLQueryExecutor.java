1315e248825b4a46c68fbcb1472a13fd2c700ac5
VanyaShestakov <vanka1108@gmail.com>
2021-05-16 00:02:46 +0300

    Added overloaded method insert with Map record param

--- src/QueryExecutor/MySQLQueryExecutor.java
+++ src/QueryExecutor/MySQLQueryExecutor.java
@@ -14,11 +14,14 @@ import java.util.*;
  * For the convenience of using the class, special parameters are introduced (records).
  * </h2>
  * <h3>
- * Record - Any class whose field names must exactly match the names of the columns in the database table.
+ * {@code record param} ({@link Recordable})- Any class whose field names must exactly match the names of the columns in the database table.
  * All fields of this class must be public.
  * The class should not have getters and setters.
  * The class must have a public full (sets values for all fields) constructor.
  * The class must implement interface {@link Recordable}
+ * </h3>
+ * <h3>
+ * {@code record param} ({@link Map}) - Any Map whose keys values must exactly match the names of the columns in the database table.
  * </h3>
  *
  */
@@ -50,12 +53,27 @@ public class MySQLQueryExecutor {
      * The record description rules for the table are specified in the class description.
      */
     public void insert(String tableName, Recordable record) throws SQLException {
-        if (connection.isClosed()) {
-            throw new ConnectionIsClosedException("Connection with database is closed");
-        }
-        if (record == null) {
-            throw new IncorrectDBRecordException("Object of DB record is null");
-        }
+        checkConnection();
+        checkRecord(record);
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM " + tableName);
+        Pair<String, String> matches = getMatches(rs.getMetaData(), record);
+        statement.execute("INSERT INTO " + tableName + " " + matches.getKey() + " VALUES " + matches.getValue());
+        statement.close();
+    }
+
+    /**This method give an opportunity to execute SQL function: <h2>INSERT</h2>
+     * @param tableName string representation of the table name;
+     * @param record a {@link Map} corresponding to a specific table.
+     *               The record description rules for the table are specified in the class description.
+     * @throws SQLException
+     * @throws ConnectionIsClosedException if connection with database is closed method throws this Exception
+     * @throws IncorrectDBRecordException if object of record does not match the corresponding table.
+     * The record description rules for the table are specified in the class description.
+     */
+    public void insert(String tableName, Map<String, Object> record) throws SQLException {
+        checkConnection();
+        checkRecord(record);
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery("SELECT * FROM " + tableName);
         Pair<String, String> matches = getMatches(rs.getMetaData(), record);
@@ -72,9 +90,7 @@ public class MySQLQueryExecutor {
      * @throws ConnectionIsClosedException
      */
     public ResultSet select(String tableName) throws SQLException, ConnectionIsClosedException {
-        if (connection.isClosed()) {
-            throw new ConnectionIsClosedException("Connection with database is closed");
-        }
+        checkConnection();
         Statement statement = connection.createStatement();
         ResultSet returnedSet = statement.executeQuery("SELECT * FROM " + tableName);
         ResultSetMetaData resultSetMetaData = returnedSet.getMetaData();
@@ -82,7 +98,6 @@ public class MySQLQueryExecutor {
         statement.close();
         return returnedSet;
     }
-
 
     /**
      * This method give an opportunity to execute SQL function: <h2>TRUNCATE (TABLE)</h2>
@@ -92,9 +107,7 @@ public class MySQLQueryExecutor {
      *      * throws this Exception
      */
     public void truncate(String tableName) throws SQLException, ConnectionIsClosedException {
-        if (connection.isClosed()) {
-            throw new ConnectionIsClosedException("Connection with database is closed");
-        }
+        checkConnection();
         Statement statement = connection.createStatement();
         statement.execute("TRUNCATE TABLE " + tableName);
         statement.close();
@@ -129,6 +142,8 @@ public class MySQLQueryExecutor {
                 fieldNames.add(columnName);
                 if (value instanceof Number) {
                     values.add(value.toString());
+                }else if (value == null) {
+                    values.add("null");
                 } else {
                     values.add("'" + value + "'");
                 }
@@ -140,12 +155,36 @@ public class MySQLQueryExecutor {
         }
         return new Pair<>(fieldNames.toString(), values.toString());
     }
-
-    public static void main(String[] args) throws SQLException, NoSuchFieldException, IllegalAccessException {
-        MySQLQueryExecutor executor = new MySQLQueryExecutor("test_database", "admin", "admin");
-        TestDBRecord record = new TestDBRecord(5, "Vasya", 34.3, "2001.11.08");
-        executor.insert("test_table", record);
-
-
+    private Pair<String, String> getMatches(ResultSetMetaData rsmd, Map<String, Object> data) throws SQLException {
+        int colCount = rsmd.getColumnCount();
+        List<Pair<String, String>> pairList = new ArrayList<>();
+        StringJoiner colNames = new StringJoiner(", ", "(", ")");
+        StringJoiner values = new StringJoiner(", ", "(", ")");
+            for (int i = 1; i <= colCount; i++) {
+                String columnName = rsmd.getColumnName(i);
+                Object value = data.get(columnName);
+                colNames.add(columnName);
+                if (value instanceof Number) {
+                    values.add(value.toString());
+                }else if (value == null) {
+                    values.add("null");
+                } else {
+                    values.add("'" + value + "'");
+                }
+            }
+        return new Pair<>(colNames.toString(), values.toString());
     }
+
+    private void checkConnection() throws SQLException {
+        if (connection.isClosed()) {
+            throw new ConnectionIsClosedException("Connection with database is closed");
+        }
+    }
+
+    private void checkRecord(Object record) {
+        if (record == null) {
+            throw new IncorrectDBRecordException("Object of DB record is null");
+        }
+    }
+
 }
